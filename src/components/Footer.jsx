434015943b5835d1dd290eba11d02414cc6ba0ec
Aman Ibrahim <amanmibra@gmail.com>
2017-10-29 11:20:30 -0400

    Added Footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Footer extends Component {
+  render() {
+    return (
+      <div className="footer">
+        &copy; 2017 Synergy, Inc. All Rights Reserved.
+      </div>
+    );
+  }
+}
+
+export default Footer;
--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Header from './../components/Header';
+import Footer from './../components/Footer';
 import Grid from './../components/Home/Grid';
 import RaisedButton from 'material-ui/RaisedButton';
 import PaymentPopUp from './../components/PaymentPopUp';
@@ -16,6 +17,7 @@ class Home extends Component {
       <div>
         <Header />
         <Grid />
+        <Footer />
       </div>
     )
   }
